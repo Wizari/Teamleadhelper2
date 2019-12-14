fd9c4185b702a10c49ad385c8d0fc2ace4b14b67
wizari <git301398>
2019-12-15 00:26:51 +0300

    Initial

---
+++ backend/src/main/kotlin/com/wizaripost/teamleadhelper/TeamLeadHelperApplication.kt
@@ -0,0 +1,11 @@
+package com.wizaripost.teamleadhelper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamLeadHelperApplication
+
+fun main(args: Array<String>) {
+	runApplication<TeamLeadHelperApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/wizaripost/teamleadhelper/TeamLeadHelperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wizaripost.teamleadhelper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamLeadHelperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
